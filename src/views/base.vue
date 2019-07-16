ddab5bb4fc738adcc093fd3d41136245d149ff26
mriliujy <mriliujy@gmail.com>
2019-07-16 17:30:30 +0800

    feat: add install some package

---
+++ src/views/base.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- base -->
+  <div class="base"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
