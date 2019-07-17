fc1000219cb81290b1fd8b300ffe06f67a1094a9
mriliujy <mriliujy@gmail.com>
2019-07-17 18:02:28 +0800

    feat: add report api && save ajax data

--- server/index.js
+++ server/index.js
@@ -41,19 +41,45 @@ app.use(session({
  * 设置允许跨域
  * @emits CORS http request
  */
-app.all('*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  if (req.method == 'OPTIONS') {
-    res.send(200);
-  } else {
-    next();
-  }
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Headers', 'Content-type');
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS,PATCH");
+  next();
+});
+// app.all('*', (req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+//   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//   if (req.method == 'OPTIONS') {
+//     res.send(200);
+//   } else {
+//     next();
+//   }
+// });
+
+
+app.post("/error", (req, res) => {
+  const Message = mongoose.model("Message");
+  let body = req.body;
+  let data = {
+    appName: body.appName,
+    appVersion: body.appVersion,
+    platform: body.platform,
+    url: body.url,
+    type: body.detail.type,
+  };
+  console.log(data);
+  console.log("--------------");
+  let message = new Message(data);
+  message.save();
+
+  res.send(200);
 });
 
 
-const point = 9998;
+const point = 4000;
 console.log("Your server listen at " + "http://localhost:" +point + "/index");
 app.listen(point);
 
