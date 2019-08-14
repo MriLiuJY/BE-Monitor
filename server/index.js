ea21fcbeb11e23d38b87343785d691d40a34181b
mriliujy <mriliujy@gmail.com>
2019-08-14 23:02:56 +0800

    feat: add routers file

--- server/index.js
+++ server/index.js
@@ -4,33 +4,33 @@
  * @version 0.0.1
  */
 
-const fs = require("fs");
-const express = require("express");
-const bodyParser = require("body-parser");
+// const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
 const session = require('express-session');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { connect, initSchemas } = require('./database/init');
 
 const app = express();
-app.use(express.static("./"));
+app.use(express.static('./'));
 let Message;
 
-(async() => {
+(async () => {
   await connect();
 
   initSchemas();
 
-  Message = mongoose.model("Message");
+  Message = mongoose.model('Message');
 })(Message);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static("build"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('build'));
 
 app.use(session({
   secret: 'my-secret',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
 }));
 
 
@@ -38,42 +38,44 @@ app.use(session({
  * 设置允许跨域
  * @emits CORS http request
  */
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Credentials", true);
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Content-type');
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS,PATCH");
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS,PATCH');
   next();
 });
 
 /**
  * 上报监控错误请求
  * @emits error event monitor ajax request
- * @parma { request, response } 
+ * @parma { request, response }
  */
-app.post("/error", (req, res) => {
-  let body = req.body;
-  let data = {
+app.post('/error', (req, res) => {
+  const { body } = req;
+  const data = {
     appName: body.appName,
     appVersion: body.appVersion,
     platform: body.platform,
     url: body.url,
-    type: body.detail.type || "",
+    type: body.detail.type || '',
   };
-  let message = new Message(data);
+  const message = new Message(data);
   message.save();
   res.send(200);
 });
 
-app.use("/error/all", async (req, res) => {
+/**
+ * 获取全部的报错信息
+ */
+app.use('/error/all', async (req, res) => {
   const messgaes = await Message.find({}).sort({
-    'meta.createdAt': -1
+    'meta.createdAt': -1,
   });
   res.send(messgaes);
 });
 
 
 const point = 4000;
-console.log("Your server listen at " + "http://localhost:" +point + "/index");
+console.log(`Your server listen at http://localhost:${point}/index`);
 app.listen(point);
-
